fc4cf2dca05d5fe037dd166696d48d25146d17ad
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-12-07 13:14:18 -0500

    Generate boilerplate routes and controllers

--- src/controllers/apiCont.ts
+++ src/controllers/apiCont.ts
@@ -2,9 +2,71 @@ import express, {Request, Response, NextFunction} from 'express'
 import expressAsyncHandler from 'express-async-handler';
 import { body, validationResult } from 'express-validator';
 
-
-const index_get = function(req: Request, res: Response, next: NextFunction): void {
+// ACCOUNT OPERATIONS
+export const login = function(req: Request, res: Response, next: NextFunction): void {
     
 }
 
-export { index_get }
+export const logout = function(req: Request, res: Response, next: NextFunction): void {
+    
+}
+
+export const create_account = function(req: Request, res: Response, next: NextFunction): void {
+    
+}
+
+export const update_profile = function(req: Request, res: Response, next: NextFunction): void {
+    
+}
+
+// GET
+export const get_feed = function(req: Request, res: Response, next: NextFunction): void {
+    
+}
+
+export const get_post = function(req: Request, res: Response, next: NextFunction): void {
+    
+}
+
+export const get_conversations = function(req: Request, res: Response, next: NextFunction): void {
+    
+}
+
+export const get_user_conversation = function(req: Request, res: Response, next: NextFunction): void {
+    
+}
+
+// POST
+export const create_post = function(req: Request, res: Response, next: NextFunction): void {
+    
+}
+
+export const create_reply = function(req: Request, res: Response, next: NextFunction): void {
+    
+}
+
+export const create_message = function(req: Request, res: Response, next: NextFunction): void {
+    
+}
+
+// UPDATE
+export const update_post = function(req: Request, res: Response, next: NextFunction): void {
+    
+}
+
+export const update_reply = function(req: Request, res: Response, next: NextFunction): void {
+    
+}
+
+// DELETE
+export const delete_post = function(req: Request, res: Response, next: NextFunction): void {
+    
+}
+
+export const delete_reply = function(req: Request, res: Response, next: NextFunction): void {
+    
+}
+
+export const delete_message = function(req: Request, res: Response, next: NextFunction): void {
+    
+}
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,6 +3,41 @@ const router = express.Router();
 
 import * as apiCont from '../controllers/apiCont.js';
 
-router.get('/', apiCont.index_get);
+// ACCOUNT OPERATIONS
+router.post('/login', apiCont.login);
+
+router.get('/logout', apiCont.logout);
+
+router.post('/create-account', apiCont.create_account);
+
+router.post('/update-profile', apiCont.update_profile);
+
+// GET
+router.get('/feed', apiCont.get_feed);
+
+router.get('/post/:postId', apiCont.get_post);
+
+router.get('/conversations', apiCont.get_conversations);
+
+router.get('/conversations/:userId', apiCont.get_user_conversation);
+
+// POST
+router.post('/post', apiCont.create_post);
+
+router.post('/post/:postId/create-reply', apiCont.create_reply);
+
+router.post('/conversations/:userId/create-message', apiCont.create_message);
+
+// UPDATE
+router.put('/post/:postId/update', apiCont.update_post);
+
+router.put('/post/:postId/:replyId/update', apiCont.update_reply);
+
+// DELETE
+router.delete('/post/:postId/delete', apiCont.delete_post);
+
+router.delete('/post/:postId/:replyId/delete', apiCont.delete_reply);
+
+router.delete('/conversations/:userId/delete', apiCont.delete_message);
 
 export default router
